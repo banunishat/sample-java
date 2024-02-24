f2f3f8f0190d96d76580526500bd6e9897d2561a
Nisha Parveen Banu <nisha.parveenbanu@gmail.com>
2024-02-24 09:29:48 -0600

    Adding two files

---
+++ src/com/java/samples/AddTwoNumbers.java
@@ -0,0 +1,10 @@
+package com.java.samples;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Adding two numbers");
+	}
+
+}
