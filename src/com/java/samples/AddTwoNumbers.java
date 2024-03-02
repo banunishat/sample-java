1d0350a56b7139a12c56b7942109b35c56d6f522
Nisha Parveen Banu <nisha.parveenbanu@gmail.com>
2024-03-02 10:44:14 -0600

    Added swapping logic in Accounts

--- src/com/java/samples/AddTwoNumbers.java
+++ src/com/java/samples/AddTwoNumbers.java
@@ -5,6 +5,11 @@ public class AddTwoNumbers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Adding two numbers");
+int x=10,y=30,z;
+z=x;
+x=y;
+y=z;
+System.out.println("X="+x+":Y="+y);
 	}
 
 }
